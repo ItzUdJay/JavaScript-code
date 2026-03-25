429174440e14f35bbb7cd2ad007f563fa711f9c6
Emmanuel Udochukwu <udochukwue950@gmail.com>
2026-03-25 09:52:05 +0100

    seventh commit

---
+++ worldcount.js
@@ -0,0 +1,10 @@
+function wordCount(sentence) {
+//  console.log(`{i love JavaScript}`)
+//  console.log("Hello World")
+    return sentence.split(" ").length;
+                     
+}
+
+
+console.log(IloveJavaScript(3))
+console.log(HelloWorld (2))
